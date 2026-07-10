8d15d22f7d97763d8937da06530aba92550708fb
Laurenz Guevara <laurenz.guevara@gmail.com>
2026-07-10 13:35:58 +0100

    feat: add additional colours and add navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="p-4 bg-surface-secondary">
+    <div className="py-2 px-4 bg-surface-0/50 border-y border-surface-1 justify-between">
       <p>Artifacts MMO hi</p>
     </div>
   );
