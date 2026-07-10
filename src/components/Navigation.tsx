c071ca27b16e31982175c340b55a1abf81193e25
Laurenz Guevara <laurenz.guevara@gmail.com>
2026-07-10 13:43:56 +0100

    feat: add docs and github link to navigation bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Navigation() {
   return (
-    <div className="py-2 px-4 bg-surface-0/50 border-y border-surface-1 justify-between">
-      <p>Artifacts MMO hi</p>
+    <div className="bg-surface-0/50 border-y border-surface-1 justify-between">
+      <div className="flex divide-x-2 divide-surface-1 border-surface-1 text-sm">
+        <div className="flex items-center px-4">
+          <p>Artifacts MMO</p>
+        </div>
+        <Link className="py-2 px-4 hover:text-maroon" href="https://docs.artifactsmmo.com/" target="_blanl">Docs</Link>
+        <Link className="py-2 px-4 hover:text-maroon border-r border-surface-1" href="https://github.com/Laurenz-Guevara/artifacts-mmo-client" target="_blank">GitHub</Link>
+      </div>
     </div>
   )
 }
